fbb28fffb8b9e73eddd065c359887233139abaa2
Athulya <110650734+AthulyaS63-github@users.noreply.github.com>
2022-09-27 14:57:59 +0530

    commit ok

---
+++ js/script.js
@@ -0,0 +1,30 @@
+    //  name
+    var player1 = "Player 1";
+    var sum=0; 
+    var num=1;
+    var current=0;
+    function editNames() {
+        player1 = prompt("Change Player1 name");
+        document.querySelector("p.Player1").innerHTML = player1;
+    }
+  // Function to roll the dice
+function rollTheDice() {
+             document.querySelector(".img1").setAttribute("src","assets/face0.gif");
+	     setTimeout(function () {
+                 current = Math.floor(Math.random() * 6) + 1;
+  	        document.querySelector(".img1").setAttribute("src", "assets/face" + current + ".png"); 
+		if(current==1){
+				num++;
+		}
+		playGame();
+	} ,1500);
+ }
+function playGame(){
+   if(num>0){
+		$("#image").remove();
+      		sum+=current;
+      		console.log(sum);	
+     		$("#"+sum+"").append('<img src="assets/giphy.gif" alt="pic" id="image"/>');
+
+	    }
+}
